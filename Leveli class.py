33cfc7f22a2ea7d4f919f5b25f66fe89d78fd437
Marvu <marvin@ut.ee>
2019-12-07 15:17:08 +0200

    Leveli class, kust saab leveli ning muud asjad

---
+++ Leveli class.py
@@ -0,0 +1,32 @@
+from PIL import Image
+
+class Level:
+    "Leveli .png fail läheb muutujaks"
+    def __init__(self, piltFail):
+        self.piltFail = piltFail
+
+    "PIL Image'iga avab pildi, et andmed saada kätte"
+    def open_pic(self):
+        return Image.open(self.piltFail, "r") #Originaal pilt
+
+    "Funktsioon tagastab, mis ette antud positsioonil värvi data on"
+    def get_pixel_value(self, level, pos):
+        self.level = level # Image.open(level)
+        self.pos = pos # (x, y) kujul positsioon, kust soovid data't saada
+        pix_value = level.getpixel(pos) # Tagastab (r,g,b)
+        return pix_value
+
+    "Pildi downscalimiseks" \
+    "Avab leveli ning teeb etteantud suuruseks" \
+    "tagastab uue leveli"
+    def resize_level(self, new_size):
+        self.new_size = new_size # Uus (width, height)
+        piltFail = self.open_pic().copy() # Avab leveli, Image.open(level)
+        level = piltFail.resize(new_size, Image.LANCZOS) # Tagastab uue leveli uute mõõtmetega
+        return level
+
+""" ns proov
+level = Level("Leveli näide.png")
+current_pos = (25,25)
+print(level.get_pixel_value(level.open_pic(), current_pos))
+"""
